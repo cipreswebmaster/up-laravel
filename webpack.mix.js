cfd3ea62c150f65e6b71912274bc86f8379da58a
Diego Saravia <saravia__2000@hotmail.com>
2021-08-13 16:12:42 -0600

    Eliminando archivo no necesario app.css

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,11 +11,6 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js").sass(
-    "resources/scss/app.scss",
-    "public/css"
-);
-
 const archivos = ["universidad.profesion.scss", "header.scss"];
 
 archivos.forEach(function (el) {
