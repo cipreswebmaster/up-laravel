8e2c0d9bbf538713babd5e88c9b3aa4ce2f6f409
Diego Saravia <saravia__2000@hotmail.com>
2021-09-06 16:37:59 -0600

    Añadiendo rutas de api para CRUD de los POST

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Traits\HelpersTrait;
 use App\Models\Post;
+use Exception;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class PostsController extends Controller
 {
@@ -24,4 +26,62 @@ class PostsController extends Controller
     );
     return view("posts.post", compact("post"));
   }
+
+  #region API
+  /**
+   * Route: /api/add_post
+   */
+  public function add_post(Request $request) {
+    $post = $request->all();
+    $image = $request->file("image");
+    $post["image"] = rand(100000, 999999) . "." . $image->getClientOriginalExtension();
+    $image_new_path = public_path("\images\posts\post");
+    $post_created = Post::create($post);
+    
+    try {
+      $image->move($image_new_path, $post["image"]);
+    } catch (FileException $e) {
+      $post_created->delete();
+      return response()->json([
+        "success" => false,
+        "message" => $e->getMessage()
+      ]);
+    }
+
+    return response()->json([
+      "success" => true,
+      "id_post" => $post_created->id_post
+    ]);
+  }
+
+  /**
+   * Route: /api/get_posts
+   */
+  public function get_posts() {
+    $posts = array_reverse(Post::all()->toArray());
+    
+    return response()->json($posts);
+  }
+
+  /**
+   * Route: /api/delete_post
+   */
+  public function delete_post(Request $request) {
+    try {
+      $post = Post::find($request->id);
+      $post->delete();
+      $image = public_path('\images\posts\post\\' . $post->image);
+      if (file_exists($image))
+        unlink($image);
+    } catch (Exception $e) {
+      return response()->json([
+        "success" => false,
+      ]); 
+    }
+
+    return response()->json([
+      "success" => true,
+    ]);
+    }
+  #endregion
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,5 @@ class Post extends Model
     use HasFactory;
 
     protected $primaryKey = "id_post";
+    protected $guarded = [];
 }
