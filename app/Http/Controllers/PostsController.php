d92eea0bdb3f4b7f62a85bdf127fed9f8024a524
Diego Saravia <saravia__2000@hotmail.com>
2021-09-09 16:16:24 -0600

    Añadiendo palabra clave principal y configurando api de post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -41,7 +41,8 @@ class PostsController extends Controller
     $post = $request->all();
     $image = $request->file("image");
     $post["image"] = rand(100000, 999999) . "." . $image->getClientOriginalExtension();
-    $image_new_path = public_path("\images\posts\post");
+    $image_subfolder = env("APP_DEBUG") ? "\images\posts\post" : "up/images/posts/post";
+    $image_new_path = public_path() . $image_subfolder;
     $post_created = Post::create($post);
     
     try {
